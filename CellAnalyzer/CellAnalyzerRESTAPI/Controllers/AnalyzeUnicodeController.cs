8b7bf071168f08b3b0737ac29db7b3da735ff91f
Gaurav Budhiraja <gaurav.budhiraja@rsystems.com>
2022-03-03 10:52:47 +0530

    added

---
+++ CellAnalyzer/CellAnalyzerRESTAPI/Controllers/AnalyzeUnicodeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CellAnalyzerRESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalyzeUnicodeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> AnalyzeUnicode(string value)
+        {
+            if (value == null)
+            {
+                return BadRequest();
+            }
+            return CellAnalyzerSharedLibrary.CellOperations.GetUnicodeFromText(value);
+        }
+    }
+}
